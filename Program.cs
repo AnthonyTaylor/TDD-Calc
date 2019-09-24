b7fd9a696b06819f5b0bdc118a2884081d3d07ca
AnthonyTaylor <AnTaylor@Northamptonshire.gov.uk>
2019-09-24 15:57:34 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TDD_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test.RunTests();
+
+            //Console.WriteLine("Please enter a number");
+        }
+
+        public static bool IsNumber(string input)
+        {
+            try
+            {
+                int result = Int32.Parse(input);
+                return true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{input}'");
+                return false;
+            }
+        }
+        public static bool IsOperator(string input)
+        {
+            return true;
+        }
+    }
+}
---
+++ test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD_Calculator
+{
+    class test
+    {
+        public static bool IsNumberValidTest(string val)
+        {
+            if (Program.IsNumber(val) == true)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsNumberInvalidTest(string val)
+        {
+            if (Program.IsNumber(val) == false)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsOperatorValidTest(string val)
+        {  
+            if (Program.IsOperator(val) == true)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool IsOperatorInvalidTest(string val)
+        {
+            if (Program.IsOperator(val) == false)
+                return true;
+            else
+                return false;
+        }
+
+        public static void RunTests()
+        {
+            Console.WriteLine("IsNumberValidTest: "+ Convert.ToString(IsNumberValidTest("5")));
+            Console.WriteLine("IsNumberInvalidTest: " + Convert.ToString(IsNumberInvalidTest("q")));
+        }
+    }
+}
